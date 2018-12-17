6c353be4aa5fa6d69c581f42c6365181aaf5a697
MrMaladroit <jamesbeckjr@gmail.com>
2018-12-17 16:13:03 -0500

    Started Distance as a Vector2

---
+++ Block-Breaker/Assets/Scripts/PaddleMovementController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PaddleMovementController : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    void Update()
+    {
+        float mouseXPosInUnits = (Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mouseXPosInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
